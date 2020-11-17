ad4f561df6b0f92043f07870008a215b5e012202
Keita Amadou <amadou.keita@etudiant.univ-reims.fr>
2020-11-17 19:43:34 +0100

    Il faut récuperer la valeur dans rl.on()

--- clients.js
+++ clients.js
@@ -3,12 +3,37 @@ const net = require('net')
 
 const client = new net.Socket()
 
-client.connect(5000, '127.0.0.1', () => {
-  console.log('connected')
+class User{
 
-  client.write('USER dylan \n\r')
-})
+    constructor(userName, password){
+        this.userName = userName;
+        this.password = password;
+    }
+    getUserName(){
+        return this.userName;
+    }
+    getUserPassword(){
+        return this.password;
+    }
+    setUserName(){
+        client.write(`USER ${this.userName} \n\r`);
+    }
+    //A ne jamais faire dans une bd
+    setUserPassword(){
+        client.write(`${this.password} \n\r`);
+    }
+    
+    connectServer(){
+        client.connect(5000, '127.0.0.1', () => {
+          console.log('connected');
+          this.setUserName();
+        })
+        
+        client.on('data', (data) => {
+          console.log(data.toString())
+        })
+    }
+}
 
-client.on('data', (data) => {
-  console.log(data.toString())
-})
+let user1 = new User("Amadou", "azerty");
+user1.connectServer();
--- server.js
+++ server.js
@@ -1,23 +1,68 @@
-const net = require('net')
+const net = require('net');
+const fs = require('fs');
+const readline = require('readline');
 
-const server = net.createServer((socket) => {
-  console.log('new connection')
 
-  socket.on('data', (data) => {
-    const [directive, parameter] = data.toString().split(' ')
-
-    switch(directive) {
-        case 'USER':
-            // check if user exist in database
-            // if true
-            socket.write('200 successfuly connected')
-            break;
+class server{
+    constructor(){
     }
-  })
+    /*setNameUser(name){
+        this.name = name;
+    }
+    getNameDirective(){
+        return this.name;
+    }*/
+    trouveUnNom(){
+        let i = 0;
+        let name = "a";
+        const server = net.createServer((socket) => {
+        console.log('new connection')
+        
+        socket.on('data', (data) => {
+            const [directive, parameter] = data.toString().split(' ')
+            const input = fs.createReadStream("./connection.json");
+            const rl = readline.createInterface({ input });
+            const regex = new RegExp(`${parameter}`);
+            
+            // switch(directive) {
+            //     case 'USER':
+            //         // check if user exist in database
+            //         // if true
+            //         rl.on('line', (line) => {
+            //             if (line.match(regex) != null){
+            //                 this.setNameUser(line.match(regex));
+            //                 console.log(i);
+            //             }
+            //             i++;
+            //         });
+            //         this.setNameUser(name)
+            //         socket.write('200 successfuly connected')
+            //         break;
+            //     }
+            if (directive == "USER"){
+                //essayer le export
+                rl.on('line', (line) => {
+                        if (line.match(regex) != null){
+                            name = line.match(regex);
+                            //console.log(name);
+                        }
+                        
+                        i++;
+                }); 
+                const listener = rl.rawListeners('line');
+                console.log(listener[0]);
+            }
+                //set
+        })
+            
+            socket.write('Hello from server')
+    })
+        
+        server.listen(5000, () => {
+          console.log('Server started at port 5000')
+        })
+    }
+}
 
-  socket.write('Hello from server')
-})
-
-server.listen(5000, () => {
-  console.log('Server started at port 5000')
-})
+let server1 = new server();
+server1.trouveUnNom();
